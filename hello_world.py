887c13df3df2043e8ebcff9af5673543118a147f
Marcelo Cueto <marcelo.cueto@uchile.cl>
2019-06-26 15:09:29 -0400

    Add /api/greetings route & GreetingsResource to hello_world module
    - Initialize the REST api
    - Define a GreetingsResource
    - Register GreetingsResource in api

--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,16 @@ from flask import (
     Flask,
     render_template,
 )
+from flask_restful import (
+    Resource,
+    Api,
+)
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# Initialize the REST API instance
+api = Api(app)
 mongo = PyMongo(app)
 
 
@@ -25,6 +31,21 @@ def greetings():
         greeting_list=greeting_list
     )
 
+
+class GreetingsResource(Resource):
+    def get(self):
+        return [
+            {
+                "message": "hello world"
+            }
+        ]
+
+
+# Register the GreetingsResource resource on the REST API
+api.add_resource(
+    GreetingsResource,
+    '/api/greetings'
+)
 
 if __name__ == '__main__':
     app.run(
